fd620722c69c33618dac1f81ef36451c78ad9f54
bayuawe <bayuaryandi21@gmail.com>
2024-09-05 00:41:59 +0700

    feat atur koneksi ke database

--- src/page/HomeView.jsx
+++ src/page/HomeView.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import axios from "axios";
+
+
+try {
+  const data = await axios.get("/api/v1/product");
+  console.log(data);
+} catch (error) {
+  console.log(error);
+}
 
 const HomeView = () => {
   return <div>HomeView</div>;
