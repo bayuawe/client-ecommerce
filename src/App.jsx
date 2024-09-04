75043d6911bc313cdbc6cfcc10e7fe0d3933f419
bayuawe <bayuaryandi21@gmail.com>
2024-09-04 10:09:20 +0700

    routing react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//components
+import AboutView from "./page/AboutView.jsx";
+import CartView from "./page/CartView.jsx";
+import HomeView from "./page/HomeView.jsx";
+import OrderView from "./page/OrderView.jsx";
+import ProductView from "./page/ProductView.jsx";
+
+import LoginView from "./page/auth/LoginView.jsx";
+import RegisterView from "./page/auth/RegisterView.jsx";
+
+import PublicLayout from "./Layouts/PublicLayout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PublicLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomeView />,
+      },
+      {
+        path: "products",
+        element: <ProductView />,
+      },
+      {
+        path: "cart",
+        element: <CartView />,
+      },
+      {
+        path: "order",
+        element: <OrderView />,
+      },
+      {
+        path: "about",
+        element: <AboutView />,
+      },
+    ],
+  },
+  {
+    path: "login",
+    element: <LoginView />,
+  },
+  {
+    path: "register",
+    element: <RegisterView />,
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <h1>App</h1>
-      <button class="btn btn-primary">Primary</button>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
