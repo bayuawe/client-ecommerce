77f5bfe57ac7e99ad275c7bf1df9aaeecfed2d9f
bayuawe <bayuaryandi21@gmail.com>
2024-09-05 01:57:27 +0700

    show product on frontend

--- src/Layouts/PublicLayout.jsx
+++ src/Layouts/PublicLayout.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
@@ -10,7 +9,7 @@ const PublicLayout = () => {
       <Header />
       <Nav />
 
-      <main className="mx-auto max-w-6xl px-8 py-20 h-[80vh]">
+      <main className="mx-auto max-w-6xl px-8 py-20 h-auto">
         <Outlet />
       </main>
 
--- src/page/HomeView.jsx
+++ src/page/HomeView.jsx
@@ -1,16 +1,53 @@
-import React from "react";
-import axios from "axios";
-
-
-try {
-  const data = await axios.get("/api/v1/product");
-  console.log(data);
-} catch (error) {
-  console.log(error);
-}
+import customAPI from "../api";
+import { useState, useEffect } from "react";
 
 const HomeView = () => {
-  return <div>HomeView</div>;
+  const [products, setProducts] = useState([]);
+  const getProduct = async () => {
+    try {
+      const { data } = await customAPI.get("/product/?limit=3");
+      setProducts(data.dataProduct);
+      console.log(data.dataProduct);
+    } catch (error) {
+      console.error("Error mengambil produk:", error);
+    }
+  };
+
+  const priceFormat = (price) => {
+    const rupiahFormat = new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(price);
+    return rupiahFormat;
+  }
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  return (
+    <>
+      <div className="text-start pb-5">
+        <h2 className="text-3xl font-bold capitalize">
+          products list
+        </h2>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {products.map((item) => (
+          <div key={item._id} className="card bg-base-100 shadow-xl">
+            <figure>
+              <img src={item.image} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{item.name}</h2>
+              <p className="font-bold text-primary">{priceFormat(item.price)}</p>
+              <p>{item.description.substring(0, 50)}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default HomeView;
