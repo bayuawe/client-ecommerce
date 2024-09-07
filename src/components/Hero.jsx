7d5934b4ab794b1b951de27e48094dee2c3cc802
bayuawe <bayuaryandi21@gmail.com>
2024-09-07 10:29:14 +0700

    feat: make slider front page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useLoaderData, Link } from "react-router-dom";
+
+const Hero = () => {
+  const { products } = useLoaderData();
+  return (
+    <>
+      <div className="grid  lg:grid-cols-2 gap-24 items-center">
+        <div className="max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl">
+          <h1>Selamat Datang di Toko Kami</h1>
+          <p className="mt-8 max-w-xl text-xl leading-8">
+            Cari product favorit kamu
+          </p>
+          <div className="mt-8 flex gap-4">
+            <Link to="/products" className="btn btn-primary btn-lg">
+              Product List
+            </Link>
+          </div>
+        </div>
+        <div className="hidden lg:carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4">
+          {products.map((item) => (
+            <div className="carousel-item" key={item._id}>
+              <img
+                src={item.image}
+                className="rounded-box object-cover w-40 h-48"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
--- src/page/HomeView.jsx
+++ src/page/HomeView.jsx
@@ -1,9 +1,10 @@
 import customAPI from "../api";
 import CardProduct from "../components/CardProduct.jsx";
 import { useLoaderData } from "react-router-dom";
+import Hero from "../components/Hero.jsx";
 
 export const loader = async ({ request }) => {
-  const { data } = await customAPI.get("/product");
+  const { data } = await customAPI.get("/product?limit=3");
 
   const products = data.dataProduct;
   return { products };
@@ -13,7 +14,10 @@ const HomeView = () => {
   const { products } = useLoaderData();
   return (
     <>
-      <div className="text-start pb-5">
+      <div>
+        <Hero />
+      </div>
+      <div className="text-start pb-5 my-5">
         <h2 className="text-3xl font-bold capitalize">products list</h2>
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
